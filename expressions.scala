8f2b52598735a4fdca81e5ef26af0c66dc79b40c
Duane <duane.edmonds@gmail.com>
2022-11-12 21:54:38 -0800

    (automated gcp push)

--- expressions.scala
+++ expressions.scala
@@ -8,47 +8,46 @@ object Exp:
         case Unary, Binary, Stack
 
     def evaluate(exp: Seq[String]): String =
-        var stack = List[String]()
-        exp foreach {op => stack = process(stack, op)}
-        stack mkString " "
+        var st = List[String]()
+        exp foreach {op => st = process(st, op)}
+        st mkString " "
 
-    def process(stack: List[String], op: String): List[String] =
+    def process(st: List[String], op: String): List[String] =
         isCommand(op) match
             case Some(Command.Unary) =>
-                val a = stack(0).toDouble
-                List({(cmds_unary(op)(a)).toString}) :++ stack.tail
+                val a = st(0).toDouble
+                List({(cmds_unary(op)(a)).toString}) :++ st.tail
             case Some(Command.Binary) =>
-                val b = stack(0).toDouble
-                val a = stack(1).toDouble
-                List({(cmds_binary(op)(a, b)).toString}) :++ stack.slice(2, stack.length)
+                val b = st(0).toDouble
+                val a = st(1).toDouble
+                List({(cmds_binary(op)(a, b)).toString}) :++ st.slice(2, st.length)
             case Some(Command.Stack) =>
-                cmds_stack(op)(stack)
+                cmds_stack(op)(st)
             case _ => // add value to stack
-                List(op) :++ stack
+                List(op) :++ st
 
     /* unary operators */
-    var cmds_unary = HashMap[String, Double => Double]()
+    val cmds_unary = HashMap[String, Double => Double]()
     cmds_unary.put("sqrt", (a: Double) => Math sqrt a)
     cmds_unary.put("inv", (a: Double) => 1 / a)
 
     /* binary operators */
-    var cmds_binary = HashMap[String, (Double, Double) => Double]()
+    val cmds_binary = HashMap[String, (Double, Double) => Double]()
     cmds_binary.put("+", (a: Double, b: Double) => a + b)
     cmds_binary.put("-", (a: Double, b: Double) => a - b)
     cmds_binary.put("x", (a: Double, b: Double) => a * b)
     cmds_binary.put("/", (a: Double, b: Double) => a / b)
 
     /* stack manipulation */
-    var cmds_stack = HashMap[String, List[String] => List[String]]()
-    cmds_stack.put("dup", (stack: List[String]) => {stack :++ List(stack(0))})
-    cmds_stack.put("sum", (stack: List[String]) => List(stack.foldLeft(0.0){_+_.toDouble}.toString))
-    cmds_stack.put("prod", (stack: List[String]) => List(stack.foldLeft(1.0){_*_.toDouble}.toString))
+    val cmds_stack = HashMap[String, List[String] => List[String]]()
+    cmds_stack.put("dup", (st: List[String]) => {st :++ List(st(0))})
+    cmds_stack.put("sum", (st: List[String]) => List(st.foldLeft(0.0){_+_.toDouble}.toString))
+    cmds_stack.put("prod", (st: List[String]) => List(st.foldLeft(1.0){_*_.toDouble}.toString))
 
-    def isCommand(op: String): Option[Command] =
-        if (cmds_unary contains op) return Some(Command.Unary)
-        if (cmds_binary contains op) return Some(Command.Binary)
-        if (cmds_stack contains op) return Some(Command.Stack)
-        None
+    def isCommand(op: String): Option[Command] = op match
+        case op if cmds_unary contains op => Some(Command.Unary)
+        case op if cmds_binary contains op => Some(Command.Binary)
+        case op if cmds_stack contains op => Some(Command.Stack)
+        case _ => None
 
-    def formatOutput(out: String): String =
-        (out split " ").reverse mkString "\n"
+    def formatOutput(out: String): String = (out split " ").reverse mkString "\n"
