7c8807547e948e82c7379a4d60b7715b64926910
Duane <duane.edmonds@gmail.com>
2022-11-14 16:43:55 -0800

    (automated gcp push)

--- expressions.scala
+++ expressions.scala
@@ -2,16 +2,18 @@ import scala.collection.mutable.{HashMap, HashSet}
 
 object Expressions:
     @main def exp(args: String*): Unit =
-        println(s"${ formatOutput(evaluateOps(args, List[String]())) }")
+        println(s"${ formatOutput(evaluateOps(args, List())) }")
+
+    val exp_version = "0.1.0"
 
     enum Command:
-        case Unary, Binary, General
+        case Unary, Binary, General, Display
 
     object Command:
         def isCommand(op: String): Option[Command] = op match
+            case op if cmds contains op => Some(Command.General)
             case op if cmds_unary contains op => Some(Command.Unary)
             case op if cmds_binary contains op => Some(Command.Binary)
-            case op if cmds contains op => Some(Command.General)
             case _ => None
 
     val delim = " "
@@ -24,7 +26,7 @@ object Expressions:
                 case true =>
                     op match
                         case "[" => // start recording anonymous function
-                            lambda = Seq[String]()
+                            lambda = Seq()
                             recording = true
                             acc
                         case "]" => // stop recording
@@ -42,54 +44,74 @@ object Expressions:
         }
         out_st mkString delim
 
-    def processOp(op: String, s: List[String]): List[String] =
+    def processOp(op: String, st: List[String]): List[String] =
         Command.isCommand(op) match
+            case Some(Command.General) => cmds(op)(st)
             case Some(Command.Unary) =>
-                val a = s(0).toDouble
-                cmds_unary(op)(a).toString :: s.tail
+                val a = st.head.toDouble
+                cmds_unary(op)(a).toString :: st.tail
             case Some(Command.Binary) =>
-                val b = s(0).toDouble
-                val a = s(1).toDouble
-                cmds_binary(op)(a, b).toString :: s.slice(2, s.length)
-            case Some(Command.General) => cmds(op)(s)
-            case _ => op :: s // add value to stack
+                val b = st.head.toDouble
+                val a = st.tail.head.toDouble
+                cmds_binary(op)(a, b).toString :: st.tail.tail
+            case _ => op :: st // add value to stack
 
     /* unary operators */
     val cmds_unary = HashMap[String, Double => Double]()
-    cmds_unary.put("abs", (a: Double) => a.abs)
-    cmds_unary.put("inv", (a: Double) => 1 / a)
-    cmds_unary.put("sgn", (a: Double) => a.sign)
-    cmds_unary.put("sqrt", (a: Double) => Math sqrt a)
+    cmds_unary.put("abs", _.abs)
+    cmds_unary.put("inv", 1 / _)
+    cmds_unary.put("sqrt", Math sqrt _)
 
     /* binary operators */
     val cmds_binary = HashMap[String, (Double, Double) => Double]()
-    cmds_binary.put("+", (a: Double, b: Double) => a + b)
-    cmds_binary.put("-", (a: Double, b: Double) => a - b)
-    cmds_binary.put("x", (a: Double, b: Double) => a * b)
-    cmds_binary.put("/", (a: Double, b: Double) => a / b)
+    cmds_binary.put("+", _ + _)
+    cmds_binary.put("-", _ - _)
+    cmds_binary.put("x", _ * _)
+    cmds_binary.put("/", _ / _)
 
     /* stack manipulation */
     val cmds = HashMap[String, List[String] => List[String]]()
-    cmds.put("dup", (s: List[String]) => s(0) :: s)
-    cmds.put("drop", (s: List[String]) => s.tail)
-    cmds.put("io", (s: List[String]) =>
-        (1 to s(0).toInt)
+    cmds.put("dup", st => st.head :: st)
+    cmds.put("drop", _.tail)
+    cmds.put("red", st =>
+        var out_st = st
+        for _ <- 1 to st.length - 1 do
+            out_st = (evaluateOps(lambda, out_st) split delim).toList
+        out_st
+    )
+    cmds.put("roll", st => st.tail :+ st.head)
+    cmds.put("rolln", st =>
+        val n = st.head.toInt
+        var out_st = st.tail
+        for _ <- 1 to n do
+            out_st = out_st.tail :+ out_st.head
+        out_st
+    )
+    cmds.put("rot", st => (st takeRight 1) ::: (st dropRight 1))
+    cmds.put("rotn", st =>
+        val n = st.head.toInt
+        var out_st = st.tail
+        for _ <- 1 to n do
+            out_st = (out_st takeRight 1) ::: (out_st dropRight 1)
+        out_st
+    )
+    cmds.put("io", st =>
+        (1 to st.head.toInt)
         .reverse
         .map {_.toString}
-        .toList ::: s.tail
+        .toList ::: st.tail
     )
-    cmds.put("map", (s: List[String]) => s map {op => evaluateOps(lambda, List[String](op))})
-    cmds.put("prod", (s: List[String]) => List(s.foldLeft(1.0){_*_.toDouble}.toString))
-    cmds.put("sum", (s: List[String]) => List(s.foldLeft(0.0){_+_.toDouble}.toString))
-    cmds.put("swap", (s: List[String]) => s(1) :: s(0) :: s.slice(2, s.length))
+    cmds.put("map", _ map {op => evaluateOps(lambda, List[String](op))})
+    cmds.put("swap", st => st.tail.head :: st.head :: st.tail.tail)
+    cmds.put("version", exp_version :: _)
 
     /* special ops */
     val cmds_ops = HashSet[String]("[", "]", "_")
     def isSpecialOp(op: String): Boolean = cmds_ops contains op
 
 
-    def formatOutput(out: String): String =
-        (out split delim)
+    def formatOutput(output: String): String =
+        (output split delim)
         .zipWithIndex
         .reverse
         .map {(element, i) =>
