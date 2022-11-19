b0caa6b5f5d9c99726e610d4cf2beb4ddfb890b3
Duane <duane.edmonds@gmail.com>
2022-11-19 07:25:24 -0800

    (automated gcp push)

--- expressions.scala
+++ expressions.scala
@@ -8,13 +8,12 @@ object Expressions:
     val delim = " "
 
     enum Command:
-        case Binary, Const, Display, General, Unary, Variable
+        case General, Memory
 
     object Command:
         def isCommand(op: String): Option[Command] = op match
             case op if cmds contains op => Some(Command.General)
-            case op if cmds_const contains op => Some(Command.Const)
-            case op if mem contains op => Some(Command.Variable)
+            case op if mem contains op => Some(Command.Memory)
             case _ => None
 
         /* support functions */
@@ -32,65 +31,11 @@ object Expressions:
             f(a.toDouble, b.toDouble).toString :: rest
 
         /**
-         * constant functions
-         * push constant to top of stack
-         */
-        val cmds_const = HashMap[String, String]()
-        cmds_const.put("pi", Math.PI.toString)
-        cmds_const.put("e", Math.E.toString)
-
-        /**
-         * unary functions (string => string)
-         * remove top element from top of stack and push result
-         */
-        //cmds_unary.put("!", unaryDouble {a => ((1 to a.toInt) foldLeft 1.0) {_ * _.toDouble}})
-        //cmds_unary.put("abs", unaryDouble {Math.abs})
-        //cmds_unary.put("ceil", unaryDouble {Math.ceil})
-        //cmds_unary.put("chs", unaryDouble {-_})
-        //cmds_unary.put("floor", unaryDouble {Math.floor})
-        //cmds_unary.put("inv", unaryDouble {1 / _})
-        //cmds_unary.put("round", unaryDouble {a => (Math round a).toDouble})
-        ///* trigonometric functions */
-        //cmds_unary.put("sin", unaryDouble {Math.sin})
-        //cmds_unary.put("cos", unaryDouble {Math.cos})
-        //cmds_unary.put("tan", unaryDouble {Math.tan})
-        //cmds_unary.put("asin", unaryDouble {Math.asin})
-        //cmds_unary.put("acos", unaryDouble {Math.acos})
-        //cmds_unary.put("atan", unaryDouble {Math.atan})
-        ///* logarithmic functions */
-        //cmds_unary.put("ln", unaryDouble {Math.log})
-        //cmds_unary.put("log2", unaryDouble {a => (Math log10 a) / (Math log10 2)})
-        //cmds_unary.put("log", unaryDouble {Math.log10})
-        ///* conversion functions */
-        //cmds_unary.put("rad_deg", unaryDouble {Math.toDegrees})
-        //cmds_unary.put("deg_rad", unaryDouble {Math.toRadians})
-        ///* bitwise operations */
-        //cmds_unary.put("not", unaryInt {~_})
-
-        /**
-         * binary functions (string, string => string)
-         * remove two elements from top of stack and push result
-         */
-        //cmds_binary.put("^", binaryDouble {Math.pow})
-        //cmds_binary.put("%", binaryDouble {_ % _})
-        //cmds_binary.put("max", binaryDouble {Math.max})
-        //cmds_binary.put("min", binaryDouble {Math.min})
-        ///* logarithmic functions */
-        //cmds_binary.put("logn", binaryDouble {(a, b) => (Math log10 a) / (Math log10 b)})
-        ///* bitwise operations */
-        //cmds_binary.put("and", binaryInt {_ & _})
-        //cmds_binary.put("or", binaryInt {_ | _})
-        //cmds_binary.put("xor", binaryInt {_ ^ _})
-
-        /**
-         * general (stack manipulation)
+         * command definitions
          */
         val cmds = HashMap[String, List[String] => List[String]]()
-        cmds.put("+", binaryDouble(_)(_ + _))
-        cmds.put("-", binaryDouble(_)(_ - _))
-        cmds.put("x", binaryDouble(_)(_ * _))
-        cmds.put("/", binaryDouble(_)(_ / _))
-        cmds.put("abs", unaryDouble(_)(Math.abs))
+
+        /*** stack manipulation ***/
         cmds.put("cls", st => Nil)
         cmds.put("count", st => st.length.toString :: st)
         cmds.put("dup", st => st.head :: st)
@@ -101,13 +46,6 @@ object Expressions:
             .reverse
             .map {_.toString}
             .toList ::: st.tail
-        )
-        cmds.put("map", _ map {op => evaluateOps(lambda, List[String](op))})
-        cmds.put("red", st =>
-            var out_st = st
-            for _ <- st.indices.tail do
-                out_st = (evaluateOps(lambda, out_st) split delim).toList
-            out_st
         )
         cmds.put("rev", st => st.reverse)
         cmds.put("roll", st => st.tail :+ st.head)
@@ -126,18 +64,77 @@ object Expressions:
                 out_st = (out_st takeRight 1) ::: (out_st dropRight 1)
             out_st
         )
-        cmds.put("sqrt", unaryDouble(_)(Math.sqrt))
-        cmds.put("sum", st => List((st foldLeft 0.0){_ + _.toDouble}.toString))
-        cmds.put("prod", st => List((st foldLeft 1.0){_ * _.toDouble}.toString))
+        cmds.put("swap", st => st.tail.head :: st.head :: st.tail.tail)
+        cmds.put("take", _ take 1)
+        cmds.put("taken", st => st.tail take st.head.toInt)
+        /* memory usage */
         cmds.put("store", st =>
             val name :: value :: rem_st = st : @unchecked
             mem.put(name, value) // store value string in hashmap
             rem_st
         )
-        cmds.put("swap", st => st.tail.head :: st.head :: st.tail.tail)
-        cmds.put("take", _ take 1)
-        cmds.put("taken", st => st.tail take st.head.toInt)
+
+        /*** maths operations ***/
+        cmds.put("+", binaryDouble(_)(_ + _))
+        cmds.put("-", binaryDouble(_)(_ - _))
+        cmds.put("x", binaryDouble(_)(_ * _))
+        cmds.put("/", binaryDouble(_)(_ / _))
+        cmds.put("!", unaryDouble(_)(a => ((1 to a.toInt) foldLeft 1.0) {_ * _.toDouble}))
+        cmds.put("^", binaryDouble(_)(Math.pow))
+        cmds.put("%", binaryDouble(_)(_ % _))
+        cmds.put("abs", unaryDouble(_)(Math.abs))
+        cmds.put("ceil", unaryDouble(_)(Math.ceil))
+        cmds.put("chs", unaryDouble(_)(-_))
+        cmds.put("e", Math.E.toString :: _)
+        cmds.put("floor", unaryDouble(_)(Math.floor))
+        cmds.put("inv", unaryDouble(_)(1 / _))
+        cmds.put("max", binaryDouble(_)(Math.max))
+        cmds.put("min", binaryDouble(_)(Math.min))
+        cmds.put("pi", Math.PI.toString :: _)
+        cmds.put("prod", st => List((st foldLeft 1.0){_ * _.toDouble}.toString))
+        cmds.put("round", unaryDouble(_)(a => (Math round a).toDouble))
+        cmds.put("sqrt", unaryDouble(_)(Math.sqrt))
+        cmds.put("sum", st => List((st foldLeft 0.0){_ + _.toDouble}.toString))
+
+        /* trigonometric functions */
+        cmds.put("sin", unaryDouble(_)(Math.sin))
+        cmds.put("cos", unaryDouble(_)(Math.cos))
+        cmds.put("tan", unaryDouble(_)(Math.tan))
+        cmds.put("asin", unaryDouble(_)(Math.asin))
+        cmds.put("acos", unaryDouble(_)(Math.acos))
+        cmds.put("atan", unaryDouble(_)(Math.atan))
+        /* logarithmic functions */
+        cmds.put("ln", unaryDouble(_)(Math.log))
+        cmds.put("log", unaryDouble(_)(Math.log10))
+        cmds.put("log2", unaryDouble(_)(a => (Math log10 a) / (Math log10 2)))
+        cmds.put("logn", binaryDouble(_)((a, b) => (Math log10 a) / (Math log10 b)))
+
+        /*** control flow (?) ***/
+
+        /*** conversion functions ***/
+        cmds.put("rad_deg", unaryDouble(_)(Math.toDegrees))
+        cmds.put("deg_rad", unaryDouble(_)(Math.toRadians))
+
+        /*** bit operations ***/
+        cmds.put("and", binaryInt(_)(_ & _))
+        cmds.put("not", unaryInt(_)(~_))
+        cmds.put("or", binaryInt(_)(_ | _))
+        cmds.put("xor", binaryInt(_)(_ ^ _))
+
+        /*** RGB colors (?) ***/
+
+        /*** higher order functions ***/
+        cmds.put("map", _ map {op => evaluateOps(lambda, List[String](op))})
+        cmds.put("red", st =>
+            var out_st = st
+            for _ <- st.indices.tail do
+                out_st = (evaluateOps(lambda, out_st) split delim).toList
+            out_st
+        )
+
+        /*** output ***/
         cmds.put("version", exp_version :: _)
+
 
         /**
          * special ops
@@ -151,8 +148,14 @@ object Expressions:
     def evaluateOps(ops: Seq[String], st: List[String]): String =
         var recording = false
         val out_st = (ops foldLeft st) {(acc, op) =>
-            Command.isSpecialOp(op) match
-                case true =>
+            !Command.isSpecialOp(op) match
+                case true => // general case
+                    !recording match
+                        case true => processOp(op, acc)
+                        case _ => // recording
+                            lambda = lambda :+ op // append op to stored anonymous function
+                            acc
+                case _ => // special op
                     op match
                         case "[" => // start recording anonymous function
                             lambda = Seq()
@@ -163,21 +166,14 @@ object Expressions:
                             acc
                         case "_" => // evaluate anonymous function on current stack
                             (evaluateOps(lambda, acc) split delim).toList
-                        case _ => ???
-                case _ =>
-                    recording match
-                        case false => processOp(op, acc)
-                        case _ =>
-                            lambda = lambda :+ op // append op to stored anonymous function
-                            acc
+                        case _ => throw new Exception("unknown special op: " + op)
         }
         out_st mkString delim
 
     def processOp(op: String, st: List[String]): List[String] =
         Command.isCommand(op) match
             case Some(Command.General) => Command.cmds(op)(st)
-            case Some(Command.Const) => (Command cmds_const op) :: st
-            case Some(Command.Variable) => mem(op) :: st
+            case Some(Command.Memory) => mem(op) :: st
             case _ => op :: st // add value to stack
 
     def formatOutput(output: String): String =
